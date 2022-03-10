0b406297070cb95ae96d7fbd681c0eb418d2dcbd
Istytsaniya <tsaniyaisty08@gmail.com>
2022-03-10 22:04:44 +0700

    menghubungkan variabel button dan edittext dengan layout

--- app/src/main/java/com/example/sampleconstraintlayout1/MainActivity.java
+++ app/src/main/java/com/example/sampleconstraintlayout1/MainActivity.java
@@ -9,9 +9,27 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Deklarasi variabel untuk button
+    Button btnlogin;
+
+    //Deklarasi variabel untuk EditText
+    EditText edEmail,edPassword;
+
+    //Deklarasi variabel untuk menyimpan email dan password
+    String nama, password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //menghubungkan variabel btnlogin dengan componen button pada layout
+        btnlogin=findViewById(R.id.btSignin);
+
+        //menghubungkan variabel edemail dengan componen button pada layout
+        edEmail=findViewById(R.id.edEmail);
+
+        //menghubungkan variabel edpassword dengan componen button pada layout
+        edPassword=findViewById(R.id.edPassword)
     }
 }
