5cdea5c4e26bc29c07bcdf160694f1b16aa04718
Istytsaniya <tsaniyaisty08@gmail.com>
2022-03-10 21:54:05 +0700

    tambah beberapa class pada MainActivity.java

--- app/src/main/java/com/example/sampleconstraintlayout1/MainActivity.java
+++ app/src/main/java/com/example/sampleconstraintlayout1/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.sampleconstraintlayout1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
