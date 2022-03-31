18010484c78c9363db151c44eafa081684f1ce5a
Istytsaniya <tsaniyaisty08@gmail.com>
2022-03-31 23:37:02 +0700

    activity4

--- app/src/main/java/com/example/sampleconstraintlayout1/MainActivity.java
+++ app/src/main/java/com/example/sampleconstraintlayout1/MainActivity.java
@@ -1,19 +1,27 @@
 package com.example.sampleconstraintlayout1;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
     //Deklarasi variabel untuk button
-    Button btnlogin;
+    Button btnLogin;
 
     //Deklarasi variabel untuk EditText
-    EditText edEmail,edPassword;
+    EditText edemail, edpassword;
 
     //Deklarasi variabel untuk menyimpan email dan password
     String nama, password;
@@ -23,13 +31,69 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //menghubungkan variabel btnlogin dengan componen button pada layout
-        btnlogin=findViewById(R.id.btSignin);
+        //Membuat fungsi onclik pada button btnLogin
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Menyimpan input user di edittext password kedalam variabel nama
+                nama = edemail.getText().toString();
+                //Menyimpan input user di edittext password kedalam variabel password
+                password = edpassword.getText().toString();
+                //Mengeset email yang benar
+                String email = "admin@mail.com";
+                //Mengeset password yang benar
+                String pwd = "123";
 
-        //menghubungkan variabel edemail dengan componen button pada layout
-        edEmail=findViewById(R.id.edEmail);
+                //mengecek apakah isi dari Email dan Password sudah sama dengan email dan password yang sudah di set
+                if (nama.isEmpty() || password.isEmpty()) {
+                    //membuar variabel Toast dan menampilkan pesan "edittext tidak boleh kosong"
+                    Toast a = Toast.makeText(getApplicationContext(),
+                            "Email dan Password wajib diisi", Toast.LENGTH_LONG);
+                    //menampilkan toast
+                    a.show();
+                } else {
+                    //mengecek apakah isi dari Email dan Password sudah sama dengan email dan password yang sudah di set
+                    if (nama.equals(email) && password.equals(pwd)) {
+                        //membuar variabel Toast dan menampilkan pesan "Login sukses"
+                        Toast a = Toast.makeText(getApplicationContext(), "Login Succes", Toast.LENGTH_LONG);
+                        //menampilkan toast
+                        a.show();
+                        //membuat objek bundle
+                        Bundle b = new Bundle();
+                        //memasukkan value dari variabel password password dengan kunci "a" dan di masukkan kedalam bundle
+                        b.putString("a", nama.trim());
+                        //memasukkan value dari variabel password password dengan kunci "a" dan di masukkan kedalam bundle
+                        b.putString("b", password.trim());
+                        //membuat objek intent berpindah activity dari mainactivity ke ActivityHasil
+                        Intent i = new Intent(getApplicationContext(), ActivityHasil.class);
+                        //memasukkan bundle kedalam intent untuk dikirimkan ke ActivityHasil
+                        i.putExtras(b);
+                        //berpindah ke ActivityHasil
+                        startActivity(i);
+                    } else {
+                        //membuar variabel Toast dan menampilkan pesan "Login sukses"
+                        Toast a = Toast.makeText(getApplicationContext(), "Login Gagal", Toast.LENGTH_LONG);
+                        //menampilkan toast
+                        a.show();
+                    }
+                }
+            }
+        });
+    }
 
-        //menghubungkan variabel edpassword dengan componen button pada layout
-        edPassword=findViewById(R.id.edPassword)
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.mnDaftar)
+        {
+            Intent i = new Intent(getApplicationContext(), DaftarActivity.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
